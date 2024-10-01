819b9c8bfec2d9927203ce18fc34ff406740713b
Tom Arne <tom.brandvold@vy.no>
2024-10-01 10:27:17 +0200

    Formatting

--- src/scripts/send_build_status.sh
+++ src/scripts/send_build_status.sh
@@ -20,13 +20,13 @@ circleci_job_name=$(circleci env subst "${PARAM_JOB_NAME}")
 
 
 curl --request POST \
-  --url "${BUILD_STATUS_API_URL}" \ 
+  --url "${BUILD_STATUS_API_URL}" \
   --header 'Content-Type: application/json' \
   --header "x-api-key: ${BUILD_STATUS_API_KEY}" \
   --data '{
-  "repo_name": "'${repo_name}'",
-  "timestamp": "'${timestamp}'",
-  "status": "'${status}'"
-  "circleci_job_name": "'${circleci_job_name}'"
-   
+  "repo_name": "'"${repo_name}"'",
+  "timestamp": "'"${timestamp}"'",
+  "status": "'"${status}"'"
+  "circleci_job_name": "'"${circleci_job_name}"'"
+
   }'
